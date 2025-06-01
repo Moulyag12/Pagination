76898024c5d99251cc8afb12840c4fa6e165b8ba
Moulya <moulyagreddy@gmail.com>
2025-06-01 13:46:34 +0530

    changes added for making selected page active

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { useMemo } from 'react';
 function App() {
   const[products,setProducts]=useState([]);
   const[currentPage,setCurrentPage]=useState(0);
+  const[loading,setLoading]=useState(true);
   const PAGE_SIZE=10;
-   const totalSize=products.length;
-   const noofpages=Math.ceil(totalSize/PAGE_SIZE);
+  const totalSize=products.length;
+  const noofpages=Math.ceil(totalSize/PAGE_SIZE);
   
 const PaginationProduct=useMemo(()=>{
   const start=currentPage*PAGE_SIZE;
@@ -20,20 +21,24 @@ const PaginationProduct=useMemo(()=>{
   const data=await fetch('https://dummyjson.com/products?limit=500');
   const totaldata=await data.json();
     setProducts(totaldata.products);
+    setLoading(false);
   }
    const handlePagechange=(n=>{
    setCurrentPage(n);
    })
-useEffect(()=>{
-fetchData();
-  },[]);
 
-   
+   useEffect(()=>{
+    fetchData();
+     },[]);
+
+  if(loading){
+    return <div>Loading...</div>
+  } 
   return (
     <>
-    <h1>Pagination</h1>
+    <h1 className='heading'>Pagination</h1>
     <div className='elements'>{[...Array(noofpages).keys().map((n)=>(
-      <span className='page-number' key={n} onClick={()=>handlePagechange(n)}>{n}</span>
+      <span  className={`page-number ${n === currentPage ? 'active' : ''}`} key={n} onClick={()=>handlePagechange(n)}>{n}</span>
     ))]}</div>
     <div className="App">
        {
